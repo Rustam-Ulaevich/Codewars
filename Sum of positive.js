aa4e9963a34669cf15c53cacbea58e71e5b24ac9
Rustam-Ulaevich <praporglamur@gmail.com>
2022-04-29 23:12:53 +0500

    4 Reversed Strings 5 String repeat 6 Opposite number 7 Return Negative 8 Points of Reflection 9 Even or Odd 10 DNA to RNA Conversion

--- Sum of positive.js
+++ Sum of positive.js
@@ -19,3 +19,39 @@ function numberToString(num) {
 }
 
 
+// 4 Reversed Strings
+function solution(str){
+    return str.split('').reverse().join('');
+}
+
+// 5 String repeat
+function repeatStr (n, s) {
+    return s.repeat(n);
+}
+
+// 6 Opposite number
+
+function opposite(number) {
+    return(-number);
+}
+
+// 7 Return Negative
+function makeNegative(num) {
+    return -Math.abs(num);
+}
+
+// 8 Points of Reflection
+function symmetricPoint(p, q) {
+    return [2*q[0] - p[0], 2*q[1]  - p[1]];
+}
+
+// 9 Even or Odd
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+}
+
+// 10 DNA to RNA Conversion
+function DNAtoRNA(dna) {
+    return dna.split("T").join("U");
+}
+
