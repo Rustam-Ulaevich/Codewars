f3a29e57fb3335d0802d09461502154802384882
Rustam-Ulaevich <praporglamur@gmail.com>
2022-04-29 22:58:43 +0500

    2 Yes or no 3 Convert a Number to a String

--- Sum of positive.js
+++ Sum of positive.js
@@ -1,3 +1,4 @@
+//1. Sum of positive
 function positiveSum(arr) {
     let sum = 0
     for(i=0; i<arr.length; i++){
@@ -5,3 +6,16 @@ function positiveSum(arr) {
     }
     return sum
 }
+
+// 2 Yes or no
+function boolToWord( bool ){
+    if(bool === true){return 'Yes'}
+    else{return 'No'}
+}
+
+// 3 Convert a Number to a String!
+function numberToString(num) {
+    return num.toString();
+}
+
+
